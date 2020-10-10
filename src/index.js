17f3e9068037cb2e982ae9c6396ec227ceea15c9
Venkatesh <venkateshnalla94@gmail.com>
2020-10-11 01:09:00 +0530

    MongoDB setup and additional scripts with nodemon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express=require('express')
-
+const mongoose=require('mongoose')
 const app=express();
+const uri = "mongodb+srv://venkateshnalla94:Venky@007@cluster0.s5ivr.mongodb.net/<dbname>?retryWrites=true&w=majority";
+mongoose.connect(uri,{
+     useNewUrlParser: true,
+     useCreateIndex:true,
+     useUnifiedTopology: true
+})
+mongoose.connection.on('connected',()=>{
+     console.log("Connected to Mongo DB")
+})
+mongoose.connection.on('error',(err)=>{
+     console.log("Error in Connection",err)
+})
 
 app.get('/',(req,res)=>{
      res.send({
@@ -9,5 +21,5 @@ app.get('/',(req,res)=>{
 })
 
 app.listen(3000,()=>{
-     console.log("listening on port 3000 ")
+     console.log("listening on port 3000 ");
 })
