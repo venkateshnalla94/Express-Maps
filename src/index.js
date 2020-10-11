de902cd5ac6d1c268d01a76a812745141e278ac3
Venkatesh <venkateshnalla94@gmail.com>
2020-10-11 06:37:25 +0530

    .env input field removed

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.get('/',requireAuth,(req,res)=>{
 })
 
 
-const port = process.env.PORT || 3000;
+const port =  3000;
 app.listen(port, () => {
      console.log(`Listening on port ${port}`);
 });
